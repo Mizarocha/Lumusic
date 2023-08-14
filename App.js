7b3629da2ed1bc2b217c19990bae05081c04a13c
Artemiza Rocha <miza-pma@hotmail.com>
2023-08-14 19:48:57 -0300

    limpar projeto #1

--- App.js
+++ App.js
@@ -1,31 +1,11 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Image, Text } from "react-native";
+
+import { View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={require("./assets/logo-icon.png")} />
-      <Text style={styles.text}>Artemiza</Text>
-
-      <StatusBar style="auto" />
+    <View>
+      <Text>Hello World</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "black",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  image: {
-    height: 400,
-    width: 400,
-  },
-  text: {
-    color: "red",
-    fontSize: 20,
-  },
-});
